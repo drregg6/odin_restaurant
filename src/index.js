d4245f0955979f67da077580e7da157a874a4ebe
Dave Regg <drregg6@gmail.com>
2019-09-17 19:25:58 -0400

    skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('hello world!')
+/*
+
+--------------------------------------
+|                                    |
+|                                    |
+|                                    |
+--------------------------------------
+
+--------------------------------------
+
+----------   ------------   ----------
+|        |   |          |   |        |
+|        |   |          |   |        |
+|        |   |          |   |        |
+----------   ------------   ----------
+
+*/
+
+const CONTENT = document.querySelector('#content');
+console.log(CONTENT);
+
+const createElement = () => {
+  const DIV = document.createElement('div');
+  DIV.innerHTML = 'Hello world';
+
+  return DIV;
+}
+
+const HEADER = createElement();
+console.log(HEADER);
+CONTENT.appendChild(HEADER);
+
+console.log('hello world!');
